eeee52d4e706ae4339ed41f5fd11ada4553a85c1
Andy Dietz <adietz@cgshealth.com>
2020-11-21 14:45:32 -0600

    updated to base64uploadadapter

--- packages/ckeditor5-build-decoupled-document/src/ckeditor.js
+++ packages/ckeditor5-build-decoupled-document/src/ckeditor.js
@@ -5,14 +5,14 @@
 
 // The editor creator to use.
 import DecoupledEditorBase from '@ckeditor/ckeditor5-editor-decoupled/src/decouplededitor';
-
+import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
 import FontSize from '@ckeditor/ckeditor5-font/src/fontsize';
 import FontFamily from '@ckeditor/ckeditor5-font/src/fontfamily';
 import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor';
 import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor';
-import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
+
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
@@ -48,11 +48,12 @@ export default class DecoupledEditor extends DecoupledEditorBase {
 DecoupledEditor.builtinPlugins = [
 	Essentials,
 	Alignment,
+	Base64UploadAdapter,
 	FontSize,
 	FontFamily,
 	FontColor,
 	FontBackgroundColor,
-	UploadAdapter,
+
 	Autoformat,
 	Bold,
 	Italic,
